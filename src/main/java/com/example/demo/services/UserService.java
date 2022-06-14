7cab8b3fd63d430fb71a4e77bf7ed30d2e08e7b2
Gisela Donoso <gisela.donoso@23people.io>
2022-06-14 13:49:57 -0400

    [UserServiceImpl]: changed UserService.java and added UserServiceImpl.java

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -1,56 +1,14 @@
 package com.example.demo.services;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
 import com.example.demo.DTO.TokenDTO;
 import com.example.demo.DTO.UserDTO;
 import com.example.demo.models.User;
-import com.example.demo.repositories.UserRepository;
-import com.example.demo.security.JWTProvider;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    @Autowired
-    UserRepository userRepository;
+    User save(UserDTO dto);
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    TokenDTO login(UserDTO dto);
 
-    @Autowired
-    JWTProvider jwtProvider;
-
-    public User save(UserDTO dto) {
-        Optional<User> user = userRepository.findByName(dto.getName());
-        if (user.isPresent())
-            return null;
-        String password = passwordEncoder.encode(dto.getPassword());
-        User newUser = User.builder()
-                .name(dto.getName())
-                .password(password)
-                .build();
-        return userRepository.save(newUser);
-    }
-
-    public TokenDTO login(UserDTO dto) {
-        Optional<User> user = userRepository.findByName(dto.getName());
-        if (!user.isPresent())
-            return null;
-        if (passwordEncoder.matches(dto.getPassword(), user.get().getPassword()))
-            return new TokenDTO(jwtProvider.createToken(user.get()));
-        return null;
-    }
-
-    public TokenDTO validate(String token) {
-        if (!jwtProvider.validate(token))
-            return null;
-        String name = jwtProvider.getNameFromToken(token);
-        if (!userRepository.findByName(name).isPresent())
-            return null;
-        return new TokenDTO(token);
-    }
+    TokenDTO validate(String token);
 }
